856590cf9fc37b69e86179f8cc7af91ce95ef646
Maxim Filippov <pinpips9@icloud.com>
2021-04-08 15:42:21 +0300

    added src

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       int price = 400_000_000;
+       int mile = 20;
+       int total = price / mile;
+        System.out.println("Total:" +total);
+
+
+    }
+}
